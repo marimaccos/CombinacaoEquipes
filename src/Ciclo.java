5f0042dbc47c72392dc2836c05d312c9b21c834d
marimaccos <63757871+marimaccos@users.noreply.github.com>
2022-08-28 16:57:21 -0300

    Add comments in Ciclo.java

--- src/Ciclo.java
+++ src/Ciclo.java
@@ -12,7 +12,7 @@ public class Ciclo {
         this.alunosPorEquipe = alunosPorEquipe;
     }
 
-
+    // faz a divisão aleatória das equipes do primeio ciclo
     public void divideEquipesRandom(ArrayList<Aluno> lTurma) {
 
         ArrayList<String> lAux = new ArrayList<String>();
@@ -36,52 +36,99 @@ public class Ciclo {
 
                 // pega a quantidade de alunos e armazena na equipe
                 for(int j = 0; j < alunosPorEquipe; j++) {
-                    equipeAtual.add( lAux.get( j + (i * alunosPorEquipe) )); // pega os prox alunos da lista
+                                            // pega os prox alunos da lista
+                    equipeAtual.add( lAux.get( j + (i * alunosPorEquipe) )); 
                 }
                 lEquipes.add(equipeAtual); // armaneza a equipe na lista
             }
 
-            // armazena os exColegas
+            // armazena os alunos que estão na mesma equipe na lista exColegas
 
-
-            // imprime as equipes
+            // imprime as equipes com os nomes dos alunos
             /*for () {
                 for () {
-
                 }
             }*/
 
-        // pede para o usuário definir como fazer as divisões
+        // se a divisão não for exata, pede para o usuário definir como fazer as divisões
         } else {
             ArrayList<Integer> qtdAlunos = new ArrayList<Integer>();
-            Scanner input = new Scanner(System.in);
-
+            Scanner input = new Scanner(System.in);            
+            
             System.out.println("Divisao nao exata! Digite como deseja distribuir as equipes. ");
             System.out.print("Digite quantas equipes: ");
-
             int qtdEquipes = input.nextInt();
-
+            
+            // armazena na lista quantos alunos em cada equipe
             for (int i = 0; i < qtdEquipes; i++) {
                 System.out.printf("Digite quantos alunos na equipe %d: ", i+1);
                 qtdAlunos.add(input.nextInt());
             }
+            
+            // faz as divisões e armazena os alunos na lista lEquipes
+            
+            // armazena os alunos que estão na mesma equipe na lista exColegas
 
+            // imprime as equipes com os nomes dos alunos
+            /*for () {
+                for () {
+                }
+            }*/
         }
     }
-
+    
+    // faz a divisão aleatória das equipes dos demais ciclos
     public void divideEquipes(ArrayList<Aluno> lTurma) {
-        // verifica se a divisao de equipes é igual
-        if (totalAlunos % alunosPorEquipe == 0) {
-            // equipes com mesmo numero de alunos
-            // armazenar na lista
+       
+        ArrayList<String> lAux = new ArrayList<String>();
+        ArrayList<ArrayList> lEquipes = new ArrayList<>();
 
-        } else {
-            // equipes com diferente numero de alunos
-            // calculo para saber a distribuiçao das equipes
+        // cria lista de string com nomes dos alunos
+        for (int i = 0; i < totalAlunos; i++) {
+            lAux.add(lTurma.get(i).getNome());
         }
+        
+        // verifica se a divisao de equipes é exata
+        if (totalAlunos % alunosPorEquipe == 0) {
 
-        // armazena os exColegas
+            int qtdEquipes = totalAlunos / alunosPorEquipe;
 
+            // forma as equipes conferindo
+            // verifica se os alunos já estiveram na mesma equipe antes            
+
+            // armazena os alunos que estão na mesma equipe na lista exColegas
+
+            // imprime as equipes com os nomes dos alunos
+            /*for () {
+                for () {
+                }
+            }*/
+            
+        } else {
+            ArrayList<Integer> qtdAlunos = new ArrayList<Integer>();
+            Scanner input = new Scanner(System.in);            
+            
+            System.out.println("Divisao nao exata! Digite como deseja distribuir as equipes. ");
+            System.out.print("Digite quantas equipes: ");
+            int qtdEquipes = input.nextInt();
+            
+            // armazena na lista quantos alunos em cada equipe
+            for (int i = 0; i < qtdEquipes; i++) {
+                System.out.printf("Digite quantos alunos na equipe %d: ", i+1);
+                qtdAlunos.add(input.nextInt());
+            }
+            
+            // forma as equipes conferindo
+            // verifica se os alunos já estiveram na mesma equipe antes          
+            
+            // armazena os alunos que estão na mesma equipe na lista exColegas
+
+            // imprime as equipes com os nomes dos alunos
+            /*for () {
+                for () {
+                }
+            }*/
+        }
     }
 }
 
